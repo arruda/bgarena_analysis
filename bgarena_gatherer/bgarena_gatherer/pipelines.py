e4cfb5e4786c3fb0fb25064d7e87172f83f7c97c
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2016-09-11 21:40:37 -0300

    fixing pipeline

--- bgarena_gatherer/bgarena_gatherer/pipelines.py
+++ bgarena_gatherer/bgarena_gatherer/pipelines.py
@@ -1,7 +1,7 @@
 #! -*- coding: utf-8 -*-
 
 from sqlalchemy.orm import sessionmaker
-from models import GameTablePlayerScore, GameTable, db_connect, create_tables
+from models import GameTablePlayerScore, GameTable, GameTableMoveAction, db_connect, create_tables
 
 
 class BGArenaPipeline(object):
@@ -30,13 +30,35 @@ class BGArenaPipeline(object):
         finally:
             session.close()
 
+    def save_moves_to_db(self, moves):
+        session = self.Session()
+        try:
+            for move in moves:
+                session.add(move)
+            session.commit()
+        except:
+            session.rollback()
+            raise
+        finally:
+            session.close()
+
+    def proccess_moves(self, item, spider):
+        moves = []
+        for move_item in item['moves']:
+            move = GameTableMoveAction(**move_item)
+            moves.append(move)
+
+        if moves:
+            self.save_moves_to_db(moves)
+        return item
+
     def process_item(self, item, spider):
         """
         Save game information in the database.
         This method is called for every item pipeline component.
         """
-        if 'testando' in item.keys():
-            return item
+        if spider.name == 'bgracemoves':
+            return self.proccess_moves(item, spider)
 
         if 'error' in item.keys():
             game_table_item = {
